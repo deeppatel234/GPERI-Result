a15e32c8b591ce406accb9e7ad2ab34655fb7521
Deep Patel <deeppatel234@live.com>
2017-06-05 22:43:17 +0530

    Update branch.js
    student graph bug solve

--- static/js/branch.js
+++ static/js/branch.js
@@ -272,11 +272,13 @@ $(".searchbtn").click(function() {
                 var studentSpi = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 };
                 var studentBacklog = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 };
                 _.each(data, function(value) {
-                    var sem = value.sem;
-                    sem = sem.replace("BE SEM ", "");
-                    sem = sem[0];
-                    studentSpi[sem] = value.spi;
-                    studentBacklog[sem] = value.currentsemblock;
+                    if(value.indexOf("Regular") != -1){
+                        var sem = value.sem;
+                        sem = sem.replace("BE SEM ", "");
+                        sem = sem[0];
+                        studentSpi[sem] = value.spi;
+                        studentBacklog[sem] = value.currentsemblock;
+                    }
                 });
                 graph(studentSpi, studentBacklog);
             },
